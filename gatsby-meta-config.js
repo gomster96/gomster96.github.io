7225dc3d0ae2ced75c30662b8416dc58bfbcc0ef
ByungwoongAn <gomster96@gmail.com>
2021-12-22 14:06:53 +0900

    fix : 자동베포가 안되는 이유를 찾기위한 커밋

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -5,18 +5,18 @@ module.exports = {
   introduction: `항상 노력하고 나아가는 주니어 백엔드 개발자입니다.`,
   siteUrl: `https://gomster96.github.io/`, // Your blog site url
   social: {
-    // twitter: ``, // Your Twitter account
+    twitter: ``, // Your Twitter account
     github: `gomster96`, // Your GitHub account
-    // medium: ``, // Your Medium account
-    // facebook: ``, // Your Facebook account
-    // linkedin: ``, // Your LinkedIn account
-    // instagram: ``, // Your Instagram account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+    instagram: ``, // Your Instagram account
   },
-  icon: `content/assets/favicon.ico`, // Add your favicon
+  icon: `content/assets/felog.png`, // Add your favicon
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'gomster96/gomster96.github.io/', // Your repository for archive comment
+    utterances: 'gomster96/gomster96.github.io', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
