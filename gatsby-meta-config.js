8be04d3c41291afd190a8d7efc467eaf6037b966
ByungwoongAn <gomster96@gmail.com>
2021-12-22 13:59:10 +0900

    docs : personel setting
    - 기존 stater-bee 에서 나의 블로그로 만들기 위해 파비콘 추가, config 변경 등을 했다

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,33 +1,32 @@
 module.exports = {
-  title: `Bee starter`,
-  description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  title: `Gomster`,
+  description: `항상 노력하고 나아가는 주니어 백엔드 개발자 `,
+  author: `Gomster`,
+  introduction: `항상 노력하고 나아가는 주니어 백엔드 개발자입니다.`,
+  siteUrl: `https://gomster96.github.io/`, // Your blog site url
   social: {
-    twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
-    medium: ``, // Your Medium account
-    facebook: ``, // Your Facebook account
-    linkedin: ``, // Your LinkedIn account
-    instagram: ``, // Your Instagram account
+    // twitter: ``, // Your Twitter account
+    github: `gomster96`, // Your GitHub account
+    // medium: ``, // Your Medium account
+    // facebook: ``, // Your Facebook account
+    // linkedin: ``, // Your LinkedIn account
+    // instagram: ``, // Your Instagram account
   },
-  icon: `content/assets/felog.png`, // Add your favicon
+  icon: `content/assets/favicon.ico`, // Add your favicon
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'gomster96/gomster96.github.io/', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: '',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
   },
-  ga: '0', // Add your google analytics tranking ID
+  ga: 'G-KRR13H5LQZ', // Add your google analytics tranking ID
   ad: '', // Add your google adsense publisherId `ca-pub-xxxxxxxxxx`
-
 }
